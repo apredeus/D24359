4b1b4d8ca0bc4916e293cbd6ab9e0de86a7e6be4
Alexander Predeus <predeus@gmail.com>
2024-05-25 22:16:52 +0100

    Initial commit

---
+++ commands.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+## ENA-deposited reads are already trimmed
+R1=ERR1143555_1.fastq.gz ## download: ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR114/005/ERR1143555/ERR1143555_1.fastq.gz
+R2=ERR1143555_2.fastq.gz ## download: ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR114/005/ERR1143555/ERR1143555_2.fastq.gz
+
+## change the number of CPUS to what you have available 
+## inflate the fasta from /data of this repo
+CPUS=32
+
+unicycler -t $CPUS --verbosity 2 --mode normal -1 $R1 -2 $R2 -o D24359.uni.out &> unicycler.log
+cp D24359.uni.out/assembly.fasta D24359.unicycler.fa
+
+ragout.py -t $CPUS ent_4st.rcp --outdir D24359.rag.out --refine &> ragout.log 
+cp D24359.rag.out/D24359_scaffolds.fasta D24359.ragout.fa 
+
+prokka --rawproduct --cpus $CPUS --outdir D24359.prokka.out --prefix D24359 --locustag D24359 --proteins WP_combo_v2.tlda.fa D24359.ragout.fa
